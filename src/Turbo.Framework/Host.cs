3e1109edf0ccfc9f1f3a35ffc1decd7060f73a57
Stevo John <stevehuwjohn@gmail.com>
2024-11-01 19:16:42 +0000

    WIP

--- src/Turbo.Framework/Host.cs
+++ src/Turbo.Framework/Host.cs
@@ -6,10 +6,32 @@ public class Host
 
     private int _height;
 
+    private char[,] _buffer;
+    
     public Host(int width, int height)
     {
         _width = width;
         
         _height = height;
+        
+        Reset();
+    }
+
+    private void Reset()
+    {
+        _buffer = new char[_width, _height];
+        
+        Clear();
+    }
+
+    private void Clear()
+    {
+        for (var x = 0; x < _width; x++)
+        {
+            for (var y = 0; y < _width; y++)
+            {
+                _buffer[x, y] = '░';
+            }
+        }
     }
 }
